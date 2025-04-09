358968fd6fe17073054e709e45f18aa62fdf8d20
rrouviere <ruben.rouviere@etu.umontpellier.fr>
2025-04-09 09:07:06 +0200

    fix: Do not gate app.run behind __name__ == '_main_''

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import create_app
 app = create_app()
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=2022, debug=False)
+#if __name__ == '__main__':
+app.run(host="0.0.0.0", port=2022, debug=False)
